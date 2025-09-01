2f3fd43dddb2b0c254b6af718afb3a34cef7b803
whddud13-byte <whddud13@gmail.com>
2025-09-01 21:14:23 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
-import "../styles/globals.css";'
+import "../styles/globals.css";
 
 export const metadata = {
-  title: 'FastReply',
-  description: 'AI 고객상담 자동화 솔루션',
-}
+  title: "FastReply",
+  description: "AI 고객상담 자동화 솔루션",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      {/* 여기에 className 추가 */}
+      <body className="bg-gradient-to-b from-blue-700 to-blue-500 text-white">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
